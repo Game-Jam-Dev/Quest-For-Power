f5aa55d02d42571e40d480b4839f221d1d0cf9a0
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-07-26 10:13:05 -0700

    Character is animated

--- RPG Mania/Assets/Scripts/Character/PlayerMovement.cs
+++ RPG Mania/Assets/Scripts/Character/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
     public float speed = 5f;
@@ -6,12 +7,16 @@ public class PlayerMovement : MonoBehaviour {
 
     private InputActions actions;
     private Rigidbody rb;
+    private Animator anim;
+    private SpriteRenderer sr;
     private Vector2 moveInput;
     private bool isSprinting;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
 
         actions = new InputActions();
     }
@@ -20,21 +25,34 @@ public class PlayerMovement : MonoBehaviour {
     {
         actions.Gameplay.Enable();
 
-        actions.Gameplay.Move.performed += context => moveInput = context.ReadValue<Vector2>();
-        actions.Gameplay.Move.canceled += context => moveInput = Vector2.zero;
+        actions.Gameplay.Move.performed += MoveCharacter;
+        actions.Gameplay.Move.canceled += StopCharacter;
         actions.Gameplay.Sprint.performed += context => isSprinting = true;
         actions.Gameplay.Sprint.canceled += context => isSprinting = false;
     }
 
     private void OnDisable() {
-        actions.Gameplay.Move.performed -= context => moveInput = context.ReadValue<Vector2>();
-        actions.Gameplay.Move.canceled -= context => moveInput = Vector2.zero;
+        actions.Gameplay.Move.performed -= MoveCharacter;
+        actions.Gameplay.Move.canceled -= StopCharacter;
         actions.Gameplay.Sprint.performed -= context => isSprinting = true;
         actions.Gameplay.Sprint.canceled -= context => isSprinting = false;
 
         actions.Gameplay.Disable();
 
         rb.velocity = Vector3.zero;
+    }
+
+    private void MoveCharacter(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        anim.SetBool("Moving", true);
+
+        sr.flipX = moveInput.x > 0;
+    }
+    private void StopCharacter(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+        anim.SetBool("Moving", false);
     }
 
     private void FixedUpdate()
